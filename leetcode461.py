baf65de8485a384ff3e838b03018f04a2b93b49f
HawkinYap <Gallowsgogo@gmail.com>
2019-08-15 16:23:38 +0800

    leetcode Q461

---
+++ leetcode461.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        n = x ^ y
+        print(n)
+        n = bin(n)
+        print(n)
+        return(n.count('1'))
+
+if __name__ == '__main__':
+    x = 1
+    y = 4
+    solution = Solution()
+    print(solution.hammingDistance(x, y))
