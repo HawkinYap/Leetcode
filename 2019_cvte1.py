b582a056d8a7db407c2892d500e6a41b07386bd3
HawkinYap <Gallowsgogo@gmail.com>
2019-09-06 09:05:59 +0800

    cvte

--- 2019_cvte1.py
+++ 2019_cvte1.py
@@ -1,3 +1,4 @@
+import time
 class Solution(object):
     def countNumber(self, s):
         """
@@ -5,15 +6,34 @@ class Solution(object):
         :rtype: int
         """
         c = 0
-        b = sorted(set(s))
+        b = set(s)
         for i in b:
             d = s.count(i)
             if d != 1:
                 c += 1
         return(len(set(s)) + c)
+        # ch_num_dict = {}
+        # for ch in s:
+        #     if ch in ch_num_dict:
+        #         ch_num_dict[ch] += 1
+        #     else:
+        #         ch_num_dict[ch] = 1
+
+        # result = 0
+        # for k, v in ch_num_dict.items():
+        #     if v == 1:
+        #         result += 1
+        #     else:
+        #         result += (1 + v)
+
+        # return(result)
+
         
         
 if __name__ == '__main__':
+    time_start=time.clock() 
     s = ['a', 'a', 'b', 'b', 'e', 'e', 'e']
     solution = Solution()
     print(solution.countNumber(s))
+    time_end=time.clock()
+    print('totally cost',time_end-time_start)
--- 2019_cvte2.py
+++ 2019_cvte2.py
@@ -1,15 +1,34 @@
+import time
 class Solution(object):
     def countNumber(self, s):
         """
         :type s: str
         :rtype: int
         """
-        for i in range(len(s)):
-            if s.count(s[i]) == 1:
-                return(i)
+        # for i in range(len(s)):
+        #     if s.count(s[i]) == 1:
+        #         return(i)
+
+        # return([i for i in range(len(s)) if s.count(s[i]) == 1][0])
+        ch_num_dict = {}
+        for ch in s:
+            if ch in ch_num_dict:
+                ch_num_dict[ch] += 1
+            else:
+                ch_num_dict[ch] = 1
+        for i, ch in enumerate(s):
+            if ch_num_dict[ch] == 1:
+                print(i)
+                break
+        else:
+            print(-1)
+
         
         
 if __name__ == '__main__':
+    time_start=time.clock() 
     s = "happyhacker"
     solution = Solution()
     print(solution.countNumber(s))
+    time_end=time.clock()
+    print('totally cost',time_end-time_start)
