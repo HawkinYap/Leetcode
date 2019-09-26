62bdc2464236b93bb0e823ffda90c00cacf612cb
HawkinYap <Gallowsgogo@gmail.com>
2019-09-26 20:43:09 +0800

    leetcode Q412

---
+++ leetcode412.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                res.append("FizzBuzz")
+            elif i % 5 == 0:
+                res.append("Buzz")
+            elif i % 3 == 0:
+                res.append("Fizz")
+            else:
+                res.append(str(i))
+        return(res)
+                
+
+if __name__ == '__main__':
+    n = 15
+    s = Solution()
+    print(s.fizzBuzz(n))
