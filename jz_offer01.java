882484930b3439bca33e5c34bda1c97f83b16eaf
Hawkin <hawkin@192.168.1.10>
2020-03-06 11:59:00 +0800

    jz_offer01 java

--- jz_offer01.java
+++ jz_offer01.java
@@ -1,6 +1,32 @@
 public class jz_offer01{
+    public boolean find(int[][] array, int target){
+        int rows = array.length;
+        int cols = array[0].length;
+        if (array != null && rows > 0 && cols >0) {
+            int row = 0;
+            int col = cols - 1;
+            while (row < rows && col >= 0) {
+                if (array[row][col] == target) {
+                    return true;
+                }
+                else if (array[row][col] > target) {
+                    col--;
+                }
+                else {
+                    row++;
+                }
+            }
+            return false;
+        }
+        else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("xxxx");
-        
+       int array[][] = {{1, 2, 8, 9}, {2, 4, 9, 12},{4, 7, 10, 13},{6, 8, 11, 15}};
+       int target = 7;
+       jz_offer01 s = new jz_offer01();
+       boolean result = s.find(array, target);
+       System.out.print(result); 
     }
 }
